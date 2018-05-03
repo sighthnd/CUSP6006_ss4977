6946b689799430cd220ff0ad833402213deff1f3
Scott Smith <smith.scott1007@gmail.com>
2018-05-03 06:03:49 -0400

    main script

---
+++ HW3/DV3_HW3_ss4977.py
@@ -0,0 +1,28 @@
+import urllib
+import altair as alt
+import json
+from flask import Flask, Response
+from HW3_analysis import loadData, createChart
+
+data = loadData()
+
+app = Flask(__name__, static_url_path='', static_folder='.')
+app.add_url_rule('/', 'root', lambda: app.send_static_file('tmp.html'))
+
+@app.route('/vis/<zipcode>')
+def visualize(zipcode):
+    if zipcode in data.keys():
+        response = createChart(data[zipcode]).to_json()
+    else:
+        response = ''
+    print response
+    return Response(response,
+        mimetype='application/json',
+        headers={
+            'Cache-Control': 'no-cache',
+            'Access-Control-Allow-Origin': '*'
+        }
+    )
+
+if __name__ == '__main__':
+    app.run(port=8002)
